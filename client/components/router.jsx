e40fdb5a1b6730385dd0924cf656dcaeb3f0e730
Vinny <vincentmichaelsmith@gmail.com>
2022-04-25 17:05:36 -0600

    added slash to path

--- client/components/router.jsx
+++ client/components/router.jsx
@@ -19,7 +19,7 @@ export const Router = () => {
       <Route path="admin" element={<Admin />} />
       <Route path="signin" element={<SignIn />} />
       <Route path="signup" element={<SignUp />} />
-      <Route path="lists/:listId" element={<List />}/>
+      <Route path="/lists/:listId" element={<List />}/>
     </Routes>
   );
 };
